f959e6520dfde2499efe0a1418c210d7cf0c0a1c
phillipdunn <phillipdunn@hotmail.com>
2022-04-02 11:33:02 +0100

    Fix - refactor app scss setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import './App.css';
 import HomePage from './pages/HomePage';
 import { menuItems } from './assets/menuItems';
 

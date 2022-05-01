9540304d13800c281a9746e299d586184cdafb42
phillipdunn <phillipdunn@hotmail.com>
2022-05-01 13:03:36 +0100

    feat: add redirect sign in

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,18 +1,34 @@
 import { Button } from '@mui/material';
-import React from 'react';
-import { signInWithPopupGoogle, createUserDocFromAuth } from '../../utils/firebase/firebase.utils';
+import React, { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import {
+  signInWithPopupGoogle,
+  signInWithRedirectGoogle,
+  createUserDocFromAuth,
+  auth,
+} from '../../utils/firebase/firebase.utils';
 import styles from './Signin.module.scss';
 
 const Signin = () => {
+  useEffect(async () => {
+    const res = await getRedirectResult(auth);
+    if (res) {
+      const userDocRef = await createUserDocFromAuth(res.user);
+    }
+  }, []);
+
   const loginGoogleUser = async () => {
     const { user } = await signInWithPopupGoogle();
-    createUserDocFromAuth(user);
+    const userDocRef = await createUserDocFromAuth(user);
   };
 
   return (
     <>
       <Button variant="contained" onClick={loginGoogleUser}>
         Google Sign in
+      </Button>
+      <Button variant="contained" onClick={signInWithRedirectGoogle}>
+        Google Sign in redirect
       </Button>
     </>
   );

ff113095fffcb7971d26333030ddaa64a8db81f2
phillipdunn <phillipdunn@hotmail.com>
2022-05-02 11:29:22 +0100

    fix: tidy up sign up, delete unused button

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,22 +1,10 @@
 import { Button } from '@mui/material';
-import React, { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-import {
-  signInWithPopupGoogle,
-  signInWithRedirectGoogle,
-  createUserDocFromAuth,
-  auth,
-} from '../../utils/firebase/firebase.utils';
+import React from 'react';
+import { signInWithPopupGoogle, createUserDocFromAuth } from '../../utils/firebase/firebase.utils';
+import SignUp from '../SignUp/SignUp';
 import styles from './Signin.module.scss';
 
 const Signin = () => {
-  useEffect(async () => {
-    const res = await getRedirectResult(auth);
-    if (res) {
-      const userDocRef = await createUserDocFromAuth(res.user);
-    }
-  }, []);
-
   const loginGoogleUser = async () => {
     const { user } = await signInWithPopupGoogle();
     const userDocRef = await createUserDocFromAuth(user);
@@ -27,9 +15,7 @@ const Signin = () => {
       <Button variant="contained" onClick={loginGoogleUser}>
         Google Sign in
       </Button>
-      <Button variant="contained" onClick={signInWithRedirectGoogle}>
-        Google Sign in redirect
-      </Button>
+      <SignUp />
     </>
   );
 };
--- src/components/SubmitButton/SubmitButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import SubmitButton from "./SubmitButton";
-
-describe("SubmitButton tests", () => {
-  it("should render", () => {
-    expect(render(<SubmitButton />)).toBeTruthy();
-  });
-});

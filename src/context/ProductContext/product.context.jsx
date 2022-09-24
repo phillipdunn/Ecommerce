7aba512ac4c23245c001ab1a53d0661398f523cb
phillipdunn <phillipdunn@hotmail.com>
2022-09-24 10:24:58 +0100

    refactor: product context

--- src/context/ProductContext/product.context.jsx
+++ src/context/ProductContext/product.context.jsx
@@ -1,12 +1,11 @@
 import { createContext, useState } from 'react';
-import PRODUCTS from '../../shop-data';
 
 export const ProductContext = createContext({
   products: [],
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
-  const value = { products };
+  const [products, setProducts] = useState();
+  const value = { products, setProducts };
   return <ProductContext.Provider value={value}>{children}</ProductContext.Provider>;
 };
